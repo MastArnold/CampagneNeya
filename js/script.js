0ec03b8b0eae7c8f02a6eb1f77117da9972a81e4
Arnold KIENOU <arnoldkienou@gmail.com>
2023-04-06 13:49:50 +0000

    second release

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+    const navbar = document.querySelector(".navbar");
     const presentationTabItem = document.querySelectorAll(".presentation-tab-item");
     //
     const presentationButton1 = document.querySelector("#presentation-button-1");
@@ -8,6 +9,11 @@
     const presentationTabItem2 = document.querySelector("#carousel-tab-2");
     const presentationTabItem3 = document.querySelector("#carousel-tab-3");
     //
+    let videoIteration = 0;
+    const buttonNextVideo = document.querySelector(".section-video-campagne .arrow-right button");
+    const buttonPreviousVideo = document.querySelector(".section-video-campagne .arrow-left button");
+    //
+    const videos = document.querySelectorAll(".video");
 
     presentationButton1.addEventListener('click', ()=>{
         presentationButton2.classList.remove('active');
@@ -36,3 +42,54 @@
         });
     });
 
+    window.addEventListener('scroll', ()=>{
+        if(document.documentElement.scrollTop > 118){
+            navbar.classList.add("down");
+        }else{
+            navbar.classList.remove("down");
+        }
+    });
+
+    buttonNextVideo.addEventListener('click', ()=>{
+        if(videoIteration < 2)
+            videoIteration++;
+        setTimeout(()=>{
+            videos.forEach((item)=>{
+                switch(videoIteration){
+                    case 0:
+                        item.style = "transform:translateX(100%)";
+                        break;
+                    case 1:
+                        item.style = "transform:translateX(0)";
+                        break;
+                    case 2:
+                        item.style = "transform:translateX(-100%)";
+                        break;
+                    default:
+                        break;
+                }
+            });
+        },0);
+    });
+
+    buttonPreviousVideo.addEventListener('click', ()=>{
+        if(videoIteration > 0)
+            videoIteration--;
+        setTimeout(()=>{
+            videos.forEach((item)=>{
+                switch(videoIteration){
+                    case 0:
+                        item.style = "transform:translateX(100%)";
+                        break;
+                    case 1:
+                        item.style = "transform:translateX(0)";
+                        break;
+                    case 2:
+                        item.style = "transform:translateX(-100%)";
+                        break;
+                    default:
+                        break;
+                }
+            });
+        },0);
+    });
---
+++ js/sms.js
@@ -0,0 +1,20 @@
+function send_sms(number, message){
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://www.lesmsbus.com:7170/ines.smsbus/smsbusMt?to=226"+number+"&text='"+message+"'&username=JoFastco&password=332GabHASMS&from=Lawyer", true);
+    xhr.responseType = "text";
+    xhr.send();
+
+    xhr.onload = function(){
+        if(xhr.status != 200){
+            alert("Erreur" + xhr.status + " : " + xhr.statusText);
+        }else{
+            var response = xhr.responseText;
+            alert(response);
+        }
+    };
+    
+    xhr.onerror = function(error){
+        alert("Echec de l'envoi ! Impossible d'envoyer votre requête. Vérifiez votre connexion et réessayez..." + error);
+    }
+}
+//send_sms('52077500', "Vous avez reçu un message de : \nKIENOU ARNOLD ; \nTel : 74166439. \n\n<< Je vais grandir un jour, Inshallah ! >>");
